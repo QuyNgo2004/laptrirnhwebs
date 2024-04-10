e939cd18e9438702cba220af3862149a2e993693
QuyNgo2004 <quyngocompurter2601@gmail.com>
2024-04-11 03:32:45 +0700

    v

--- example-app/resources/views/crud_user/create.blade.php
+++ example-app/resources/views/crud_user/create.blade.php
@@ -1,14 +1,14 @@
-
 @extends('dashboard')
+
 @section('content')
     <main class="signup-form">
         <div class="cotainer">
             <div class="row justify-content-center">
                 <div class="col-md-4">
                     <div class="card">
-                        <h3 class="card-header text-center">Register User</h3>
+                        <h3 class="card-header text-center">Create User</h3>
                         <div class="card-body">
-                            <form action="{{ route('register.custom') }}" method="POST">
+                            <form action="{{ route('user.postUser') }}" method="POST">
                                 @csrf
                                 <div class="form-group mb-3">
                                     <input type="text" placeholder="Name" id="name" class="form-control" name="name"
@@ -31,13 +31,8 @@
                                         <span class="text-danger">{{ $errors->first('password') }}</span>
                                     @endif
                                 </div>
-                                <div class="form-group mb-3">
-                                    <div class="checkbox">
-                                        <label><input type="checkbox" name="remember"> Remember Me</label>
-                                    </div>
-                                </div>
                                 <div class="d-grid mx-auto">
-                                    <button type="submit" class="btn btn-dark btn-block">Sign up</button>
+                                    <button type="submit" class="btn btn-dark btn-block">Submit</button>
                                 </div>
                             </form>
                         </div>
